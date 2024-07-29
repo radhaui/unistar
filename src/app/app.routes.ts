4d9190bbebde4835fe7b0f633fb304b7b420e1be
radhaui <radhaui1612@gmail.com>
2024-07-29 15:18:56 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './servicess/auth.guard';
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent,canActivate: [AuthGuard] },
+    { path: '**', redirectTo: '/login' }
+]
